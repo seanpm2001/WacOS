2580b4a7a19569cd13757bef2091fa809a8b812e
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-27 17:54:10 -0700

    DMG_SupportV1.swift
    Version 1 of the DMG support Swift file. Highly incomplete, it is just a template of where things should go, and how files should be named. This whole project is still just a concept.

---
+++ LIBraries/x64/OldVersions/DMG_SupportV1.swift
@@ -0,0 +1,10 @@
+// Start of script
+/*
+This library is for the support of DMG files on 64 bit systems 
+*/
+
+// Variabel section
+var dmgFile(".dmg");
+var maxLength(9223372036854775807); // Max value for a signed 64 bit integer
+
+// End of script
