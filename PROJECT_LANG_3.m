0fc1a1880d1c82b5c10ad6f5c7b0657ce6893e97
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-27 19:54:13 -0700

    PROJECT_LANG_3.m
    The current version (version 1) of the 3rd project language file for this project.

---
+++ PROJECT_LANG_3.m
@@ -0,0 +1,21 @@
+@interface classname : projectLanguageFileTwo {
+  // Start of script
+  // instance variables
+}
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+param2_callName:(param2_type)param2_varName;
+  @MacOS_10_1_ObjectiveC classname : superclassname {
+    // I chose Objective-C as the third project language file for this project (WacOS) as it is a language to better emulate the Apple environment, and is 3rd in line with the other C's (C, C++) although C# won't be used in this project.
+	NSLog(@"I chose Objective-C as the third project language file for this project (WacOS) as it is a language to better emulate the Apple environment, and is 3rd in line with the other C's (C, C++) although C# won't be used in this project.");
+}
+@end
+/* File info
+* File version: `1 (Tuesday, April 27th 2021 at 7:54 pm)
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 22
+* This script is incomplete, and needs lots of work */
+// End of script
