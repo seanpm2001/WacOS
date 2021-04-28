71342a97b6826a6241055a965eb45ea3e462cc94
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-27 20:07:31 -0700

    PROJECT_LANG_7_V1.swift
    An archive of the first version of the 7th project language file for this project.

---
+++ PROJECT_LANG_7_V1.swift
@@ -0,0 +1,14 @@
+// Start of script
+// I have chosen Swift as the 7th project language file for this project, as it is also needed for Apple virtualization and software development, and is less universal than Python.
+class projectLanguageFile {
+var str = "I have chosen Swift as the 7th project language file for this project, as it is also needed for Apple virtualization and software development, and is less universal than Python.";
+myValue = @String
+let myValue = str();
+return myValue;
+break;
+}
+// I don't know how to make working strings in Swift
+// File version: 1 (Tuesday, April 27th 2021 at 8:07 pm)
+// File type: Swift 5.3 Source file (*.swift)
+// Line count (including blank lines and compiler line): 15
+// End of script
