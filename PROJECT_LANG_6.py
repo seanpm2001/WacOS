51d5059d302a4785c8976ec3e5a3a2d8f4d1a5ea
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-27 20:03:16 -0700

    PROJECT_LANG_6.py
    The current version (version 1) of the 6th project language file for this project.

---
+++ PROJECT_LANG_6.py
@@ -0,0 +1,18 @@
+# Start of script
+# I have decided to make Python 3 the 6th project language for this project (WacOS) as it is good for application development, and is the last non-Apple language as a main language used on this project (noted: Objective-C++/Objective-C)
+''' '''
+class projectLanguageFile()
+def projectLanguageFileDesc():
+	print("Project Language File\n")
+	print("For: WacOS (Linux Distribution)")
+	print("I have decided to make Python 3 the 6th project language for this project (WacOS) as it is good for application development, and is the last non-Apple language as a main language used on this project (noted: Objective-C++/Objective-C)")
+projectLanguageFileDesc()
+noMore = input("Press [ENTER] key to quit")
+print("The program should now be closed. If the program is still running, try closing the window with the close button. If this doen't work, end the task/process with a task/proces manager")
+break
+""" """
+# File info
+# File version: 1 (Tuesday, April 27th 2021 at 8:03 pm)
+# File type: Python 3 source file (*.py)
+# Line count (including blank lines and compiler line): 19
+# End of script
