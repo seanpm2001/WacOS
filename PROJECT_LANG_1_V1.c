1d319f8d35e9d79c32ac7f924eb29b04b850f57f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-27 19:48:35 -0700

    PROJECT_LANG_1_V1.c
    An archive of the first version of the first project language file for this project.

---
+++ PROJECT_LANG_1_V1.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+// Start of script
+// I decided to make the main project language for this project (WacOS) the C programming language, as it is the most needed language for system development (especially on UNIX) and is needed for direct system access.
+union projectLanguageFile() {
+int main(void) {
+    printf("Project language file 1: C");
+    printf("For: WacOS (Linux Distribution)");
+    printf("I decided to make the main project language for this project (WacOS) the C programming language, as it is the most needed language for system development (especially on UNIX) and is needed for direct system access.\n");
+    break;
+  }
+}
+int loop1 = 1;
+while (loop1 == 1) {
+  return main();
+  break;
+}
+else {
+  break;
+}
+printf("Error. Loop has broken. Please close the program");
+}
+// File info
+// File version: 1 (Tuesday, April 27th 2021 at 7:48 pm)
+// File type: C Source file (*.c)
+// Line count (including blank lines and compiler line): 27
+// End of script
