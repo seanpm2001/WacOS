fe9235f8514d926c124e0292dbc4492cc415a044
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-27 19:57:01 -0700

    PROJECT_LANG_4.asm
    The 4th project language file for this project.

---
+++ PROJECT_LANG_4.asm
@@ -0,0 +1,6 @@
+; I have decided to make Assembly the fourth project language for this project (WacOS) as it is needed to emulate some hardware elements, and is going to be a somewhat common language for this project.
+
+; File info
+; File version: 1 (Tuesday, April 27th 2021 at 7:57 pm)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 7
