dc53127f8210dc141b4cbe9640c847e4a374eedc
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-27 20:00:23 -0700

    PROJECT_LANG_5.vhdl
    The 5th project language file for this project.

---
+++ PROJECT_LANG_5.vhdl
@@ -0,0 +1,6 @@
+-- Start of script
+-- I have chosen VHDL as the 5th project language file for this project (WacOS) as along with Assembly, it is needed for hardware virtualization.
+-- File version: 1 (Tuesday, April 27th 2021 at 8:00 pm)
+-- File type: Virtual Hard Disk Language (VHDL) Source file (*.vhdl, *.vhd, *.vdi)
+-- Line count (including blank lines and compiler line): 7
+-- End of script
