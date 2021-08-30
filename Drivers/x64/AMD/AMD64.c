d3e16665d92004def6e058f9df3b59fc496dda69
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-30 15:56:04 -0700

    AMD64.c
    The script for AMD 64 Bit processors.

---
+++ Drivers/x64/AMD/AMD64.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+// Start of script
+// AMD 64 bit driver source script
+// This is horrible code and needs significant improvement.
+// I am not very good at programming in C. This may appear as bad code and may not compile correctly.
+// Definitions
+union AMDX64();
+// Main
+int main(void) {
+    x = int(1)
+    return LinuxKrnl();
+    return x64Test();
+    return amd64();
+    return configAMD();
+    printf("Checking for AMD (x64) support");
+    break;
+    exit;
+} // End of main method
+int x64Test(void) {
+ // 64 bit test
+  cl = float(2 ** 64);
+  printf(bz); // It is named CL, as AZ == 26, BZ = 52, CL = 64
+  if (error == false):
+    return x64();
+  else:
+    printf("This is not a 64 bit AMD device");
+}
+int x64(void) {
+ error == ("true");
+ x64Dev = bool("undecided"); // Temporary, I don't know how to declare a boolean without giving it a value, so for now, it has an illegal value
+ wait(5)
+ if error == ("false"); {
+   x64Dev == true;
+   break
+ } elif (x == x): {
+    x64Dev == "false";
+    error == "true";
+    return error();
+    break;
+    exit;
+  }
+}
+return main();
+break;
+exit;
+/* File info
+* File version: 1 (Monday, August 30th 2021 at 3:56 pm)
+* File type: C Source file (*.c)
+* Line count (including blank lines and compiler line): 54
+*/
+// This script is incomplete, and needs lots of work
+// End of script
