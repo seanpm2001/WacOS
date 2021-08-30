dcc78adea51c0fbd8d44719ff96cd904a404fdd3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-30 16:21:04 -0700

    MAIN.cpp
    The main script for this subproject.

---
+++ SirIsaacNewtonScript/MAIN.cpp
@@ -0,0 +1,26 @@
+// Start of script
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+// Main script for Sir Isaac Newton Script
+// An open source recreation of NewtonScript
+// Please note that I am not experienced with C++ so this file may not be functional and may come off as bad code
+class isaac() {
+  int about() {
+   std::cout >> "About Sir Isaac NewtonScript\n";
+   std::cout >> "Sir Isaac Newton OS is a recreation of NewtonOS by Apple, that was originally used on the Apple Newton series of tablets produced by Apple in the early 1990s\n";
+   std::cout >> "Sir Isaac Newton Script is a recreation of NewtonScript, which is a C++ derived language that was used to program NewtonOS. This project uses Isaac Newtons full surname, including the honorary Sir title.\n");
+   std::cout >> "Like NewtonScript, SirIsaacNewtonScript is also C++ derived, and is designed to be as light as possible. It is a 8 bit and 16 bit hybrid operating system part of the WacOS project.\n"; 
+   std::cout >> "End of abouut section";
+   break;
+  }
+  return about();
+  break;
+  exit;
+}
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx)
+* File version: 1 (Monday, Apugust 30th 2021 at 4:21 pm)
+* Line count (including blank lines and compiler line): 27
+*/
+// End of script
