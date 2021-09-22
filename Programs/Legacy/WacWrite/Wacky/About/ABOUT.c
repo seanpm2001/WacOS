854e186b42d21149794386eeafbc84a1ec1c198c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-22 14:51:13 -0700

    ABOUT.c
    The about dialog box script for Wacky Mode on WacWrite.

---
+++ Programs/Legacy/WacWrite/Wacky/About/ABOUT.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+// Start of script
+// The about page for WacWrite (Wacky mode)
+// I am not very good at programming in C. This may appear as bad code and may not compile correctly.
+// Definitions
+union aboutWacWriteModeLegacy ("Classic");
+// Main
+int main(void) {
+    return logo();
+    printf("About WacWrite (Wacky Mode)\nVersion: 0.01 (Alpha) - 2021 Wednesday September 22nd\nDescription unavailable");
+    return authorsButton();
+    return licenseButton();
+    return continueButton();
+    break;
+    exit;
+}
+int logo(); {
+  // Logo file goes here
+  // The logo goes in its own space to the left of the about text
+  scanf("/Programs/Legacy/WacWrite/Wacky/LOGO.BMP");
+  break;
+}
+int authorsButton(); {
+  // The authors button for the about dialog box
+  // The button should be placed to the left of the license button
+  printf("[Authors]");
+  continue; {
+    printf("Authors");
+    printf("@seanpm2001 / @WacOS-dev");
+    printf("No other authors to list at the moment");
+    break;
+  }
+}
+int licenseButton(); {
+  // The license button for the about dialog box
+  // The button should be centered in the middle of the bottom of the dialog box
+  printf("[License]");
+  continue; {
+    printf("GNU General Public License V3");
+    printf("<one line to give the program's name and a brief idea of what it does.>");
+    printf("Copyleft (🄯) 2021 @seanpm2001 / @WacOS-dev");
+    printf(" ");
+    printf("This program is free software: you can redistribute it and/or modify");
+    printf("it under the terms of the GNU General Public License as published by");
+    printf("the Free Software Foundation, either version 3 of the License, or");
+    printf("(at your option) any later version.");
+    printf(" ");
+    printf("This program is distributed in the hope that it will be useful,");
+    printf("but WITHOUT ANY WARRANTY; without even the implied warranty of");
+    printf("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the");
+    printf("GNU General Public License for more details.");
+    printf(" ");
+    printf("You should have received a copy of the GNU General Public License");
+    printf("along with this program.  If not, see <https://www.gnu.org/licenses/>.");
+    printf(" ");
+    scanf("Continue");
+    break;
+  }
+}
+int continueButton(); {
+  // The continue button for the about dialog box
+  // The button should be centered at the far right of the bottom of the dialog box
+  printf("[Continue");
+  break;
+}
+break;
+goto 9;
+return 0;
+/* File info
+* File version: `1 (Wednesday, 2021 September 22nd at 2:51 pm)
+* File type: C Source file (*.c *.h)
+* Line count (including blank lines and compiler line): 77
+*/
+// This script is incomplete, and needs lots of work
+// End of script
