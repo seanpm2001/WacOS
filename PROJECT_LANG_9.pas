3c8870bf5b9df68ad138444a7a4d15c3f1774a28
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-24 17:43:01 -0700

    PROJECT_LANG_9.pas
    The current version (version 1) of the 9th project language file for this project.

---
+++ PROJECT_LANG_9.pas
@@ -0,0 +1,14 @@
+// Start of script
+program LanguageFile(output);
+begin
+    Write('I decided to make Pascal the ninth project language file for this project (WacOS) as some included software for this project had to be written in Pascal to maintain compatibility and similarity')
+    {No ";" is required after the last statement of a block -
+        adding one adds a "null statement" to the program, which is ignored by the compiler.}
+end.
+
+// I decided to make Pascal the ninth project language file for this project (WacOS) as some included software for this project had to be written in Pascal to maintain compatibility and similarity
+
+// File version: 1 (Friday, 2021 September 24th at 5:43 pm)
+// File type: Pascal source file (*.pas)
+// Line count (including blank lines and compiler line): 15
+// End of script
