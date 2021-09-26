efdd524db1c42488514e1b49a6a541c5c0090ee7
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-25 20:00:50 -0700

    PROJECT_LANG_19_V1.pl
    An archive of the first version of the 19th project language file for this project.

---
+++ OldVersions/PROJECT_LANGUAGE/Perl/PROJECT_LANG_19_V1.pl
@@ -0,0 +1,8 @@
+# Start of script
+# I chose Perl as the 19th project language file for this project (WacOS) as Perl is one of the languages used in the Web Stack for this project.
+print "I chose Perl as the 19th project language file for this project (WacOS) as Perl is one of the languages used in the Web Stack for this project.";
+# File info
+# File type: Perl source file (*pl)
+# File version: 1 (Saturday, 2021 September 25th at 8:00 pm)
+# Line count (including blank lines and compiler line): 9
+# End of script
