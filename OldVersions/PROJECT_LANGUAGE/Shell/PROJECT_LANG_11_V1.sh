379ef43573aad2104c291495e9626f64218947aa
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-25 18:45:48 -0700

    PROJECT_LANG_11_V1.sh
    An archive of the first version of the 11th project language file for this project.

---
+++ OldVersions/PROJECT_LANGUAGE/Shell/PROJECT_LANG_11_V1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Start of script
+# I have chosen Shell as the eleventh project language file for this project (WacOS) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project uses. I didn't consider adding it originally, but today (2021 Saturday September 25th) I decided to turn every language used into a project language file for this project.
+function projectLanguageFileEleven() {
+  echo ("I have chosen Shell as the eleventh project language file for this project (WacOS) because Shell is the go-to language for debug scripting on UNIX, and is native to the UNIX desktop/mobile scheme that this project uses. I didn't consider adding it originally, but today (2021 Saturday September 25th) I decided to turn every language used into a project language file for this project.");
+  break;
+}
+return projectLanguageFileELEVEN();
+break;
+exit;
+# File info
+# File version: 1 (Saturday, 2021 September 25th at 6:45 pm)
+# File type: Bourne Again SHell Script file (*.sh)
+# Line count (including blank lines and compiler line): 16
+# End of script
