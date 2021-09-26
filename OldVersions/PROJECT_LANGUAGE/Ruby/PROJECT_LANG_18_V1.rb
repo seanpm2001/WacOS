6852cd955b6665e204cb54ad94a2e09548791777
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-25 19:55:49 -0700

    PROJECT_LANG_18_V1.rb
    An archive of the first version of the 18th project language file for this project.

---
+++ OldVersions/PROJECT_LANGUAGE/Ruby/PROJECT_LANG_18_V1.rb
@@ -0,0 +1,20 @@
+# Start of script
+# I chose Ruby as the eighteenth project language file for this project (WacOS) as Ruby is needed for some functionality in the WacOS web server bundle.
+class project_language_file_18():
+  def projectLanguageFile5():
+    puts "Project language file 18"
+    puts "For: WacOS"
+    puts "About:"
+    puts "I chose Ruby as the eighteenth project language file for this project (WacOS) as Ruby is needed for some functionality in the WacOS web server bundle."
+    break
+  return projectLanguageFile18()
+  return 0
+  break
+end # end class
+# Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+# File info
+# File type: Ruby source file (*.rb)
+# File version: 1 (Saturday, 2021 September 25th at 7:55 pm)
+# Line count (including blank lines and compiler line): 21
+
+# End of script
