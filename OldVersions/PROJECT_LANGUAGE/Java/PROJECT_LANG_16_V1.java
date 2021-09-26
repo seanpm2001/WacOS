70799bced8a77baf0fb3cd0457912e4306041077
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-25 19:38:55 -0700

    PROJECT_LANG_16_V1.java
    An archive of the first version of the 16th project language file for this project.

---
+++ OldVersions/PROJECT_LANGUAGE/Java/PROJECT_LANG_16_V1.java
@@ -0,0 +1,21 @@
+// Start of script
+// I chose Java as the sixteenth project language file for this project (WacOS) as Java is one of the languages that this project is written in, and needs to have a maintained JRE (Java Runtime Environment) to imitate its counterpart.
+public class projectLanguageFileSixteen {
+	public static void main(String args) {
+    System.out.println("Project language 16");
+    System.out.println("For: WacOS");
+		System.out.println("I chose Java as the sixteenth project language file for this project (WacOS) as Java is one of the languages that this project is written in, and needs to have a maintained JRE (Java Runtime Environment) to imitate its counterpart.");
+    break;
+}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+main();
+int y = 16;
+System.out.println("Current project language file: " + int(y));
+break } // No exit command exists
+// I am not the best at Java, so this script may not work
+/* File info
+* File version: 1 (Saturday, 2021 September 25th at 7:38 pm)
+* File type: Java 10 Source file (*.java)
+* Line count (including blank lines and compiler line): 22
+*/
+// End of script
