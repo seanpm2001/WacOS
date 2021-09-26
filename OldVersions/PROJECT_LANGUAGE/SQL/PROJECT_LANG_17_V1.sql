62a8899384d8cebf50b0ed80762db88cc3849afd
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-25 19:50:01 -0700

    PROJECT_LANG_17_V1.sql
    An archive of the 1st version of the 17th project language file for this project.

---
+++ OldVersions/PROJECT_LANGUAGE/SQL/PROJECT_LANG_17_V1.sql
@@ -0,0 +1,12 @@
+--Start of script;
+--MySQL for old versions, PostgreSQL for later versions
+--I chose SQL as the seventeenth project language for this project (WacOS) as certain databases for this project require SQL as their base language.
+CREATE TABLE "PROJECT_LANGUAGE_FILE_17" WITH STRING "Project language file 17\nFor: WacOS\nAbout:\nI chose SQL as the seventeenth project language for this project (WacOS) as certain databases for this project require SQL as their base language.";
+SELECT * FROM PROJECT_LANGUAGE_FILE_17;
+--I am unable to test this program, as I don't have a proper database/server set up yet;
+-- File info
+-- File type: SQL source code (*.sql)
+-- File version: 1 (Saturday, 2021 September 25th at 7:49 pm)
+-- Line count (including blank lines and compiler line): 13
+-- Language: English (US)
+--End of script;
