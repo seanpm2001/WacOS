4996ad80c400c951eedcac966ca611e7520ee385
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-27 15:33:50 -0700

    PROJECT_LANG_23_V1.js
    An archive of the first version of the 23rd project language file for this project>

---
+++ OldVersions/PROJECT_LANGUAGE/JavaScript/PROJECT_LANG_23_V1.js
@@ -0,0 +1,29 @@
+// Start of script
+// I chose JavaScript as the twenty-third project language for this project (WacOS) as JavaScript is one of the languages needed for the WacOS server stack, for vanilla JavaScript, JQuery, and more. It likely won't see too much usage outside of the Server stack, unless a website is additionally included into the main repository.
+class projectLanguageFileTwentyThree() {
+  void main() {
+    console.log ("Project language file 23");
+    console.log ("WacOS");
+    console.log ("About:");
+    console.log ("I chose JavaScript as the twenty-third project language for this project (WacOS) as JavaScript is one of the languages needed for the WacOS server stack, for vanilla JavaScript, JQuery, and more. It likely won't see too much usage outside of the Server stack, unless a website is additionally included into the main repository.");
+    /* Garbage syntax, so I can test out 2 new keywords I recently learned. Uncomment to highlight
+    x = bool("true");
+    y = bool("false");
+    if (x == "false"); {
+      try y
+    } else: {
+      catch x;
+    }
+    */
+    break;
+    return 0;
+  }
+}
+return main();
+break;
+/* File info
+* File version: 1 (Monday, 2021 September 27th at 3:33 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 30
+*/
+// End of script
