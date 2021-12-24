f35f902c95dd4f2ee89017016530493e08886eac
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-23 20:04:54 -0800

    PROJECT_LANG_31_V1.ts
    An archive of the first version of the 31st project language file for this directory set.

---
+++ OldVersions/PROJECT_LANGUAGE/TypeScript/PROJECT_LANG_31_V1.ts
@@ -0,0 +1,24 @@
+// Start of script
+// I chose TypeScript as the thirty-first project language for this project (Seanpm2001/WacOS) as TypeScript is a good language for this project, as this project already uses JavaScript. TypeScript is a much safer and better compiler to JavaScript, and I want to start representing it, since valid JavaScript is valid TypeScript/valid TypeScript is valid JavaScript. I also wanted a project language file to represent this.
+class projectLanguageFileThirtyOne() {
+  void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 31");
+    alert("Project language file 31");
+    console.log ("For: Seanpm2001/WacOS/");
+    alert("For: Seanpm2001/WacOS");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose TypeScript as the thirty-first project language for this project (Seanpm2001/WacOS) as TypeScript is a good language for this project, as this project already uses JavaScript. TypeScript is a much safer and better compiler to JavaScript, and I want to start representing it, since valid JavaScript is valid TypeScript/valid TypeScript is valid JavaScript. I also wanted a project language file to represent this.");
+    alert("I chose TypeScript as the thirty-first project language for this project (Seanpm2001/WacOS) as TypeScript is a good language for this project, as this project already uses JavaScript. TypeScript is a much safer and better compiler to JavaScript, and I want to start representing it, since valid JavaScript is valid TypeScript/valid TypeScript is valid JavaScript. I also wanted a project language file to represent this.");
+    break;
+	}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+/* File info
+* File version: 1 (2021, Thursday, December 23rd at 8:04 pm)
+* File type: TypeScript source file (*.ts *.tsx)
+* Line count (including blank lines and compiler line): 25
+*/
+// End of script
