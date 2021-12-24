9b5477d28f9432e59e65052f766093b322c7e30b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-24 15:49:00 -0800

    PROJECT_LANG_34_V1.rs
    An archive of the first version of the 34th project language file for this project.

---
+++ OldVersions/PROJECT_LANGUAGE/Rust/PROJECT_LANG_34_V1.rs
@@ -0,0 +1,16 @@
+// Start of script
+// I decided to make Rust the thirty-third project language file for this project (Seanpm2001/WacOS) as Rust is a good language for writing stable programs such as web browsers. It is getting its own project language file, starting here.
+fn main() {
+  println!("Project language file 34 (Rust)");
+  println!("For: WacOS");
+  println!("About:");
+  println!("I decided to make Rust the thirty-third project language file for this project (Seanpm2001/WacOS) as Rust is a good language for writing stable programs such as web browsers. It is getting its own project language file, starting here.");
+  break;
+}
+return main();
+break;
+// File info
+// File version: 1 (Friday, 2021 December 24th at 3:48 pm)
+// File type: Rust Source file (*.rs)
+// Line count (including blank lines and compiler line): 17
+// End of script
