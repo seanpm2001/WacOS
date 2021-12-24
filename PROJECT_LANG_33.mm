f3c6c7875eca9f7091da3c721d0a47c230928b4b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-23 21:11:06 -0800

    PROJECT_LANG_33.mm (V1)
    The current version (version 1) of the 33rd project language file for this directory set.

---
+++ PROJECT_LANG_33.mm
@@ -0,0 +1,45 @@
+#!/bin/objective-cpp -x
+// Start of script
+
+// Import section
+
+#import </CoreFoundation/CFAttributedString.h>
+#import </Foundation/NSAttributedString.h>
+#import </Foundation/NSDictionary.h>
+#import </Foundation/NSMutableAttributedString.h>
+#import </UIKit/UIKit.h>
+
+// using System.DotNet;
+// using System.ObjectiveCSharp;
+
+@namespace objectivecsharp-projectLanguage {
+
+void projectLanguageFileOne(); {
+	@interface classMain : superclassMain {
+  	// instance variables
+	}
++ classMethod1;
++ (return_type)classMethod2;
++ (return_type)classMethod3:(param1_type)param1_varName;
+
+- (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+- (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+                              param2_callName:(param2_type)param2_varName;
+        // I decided to make Objective-C++ the main project language file for this project (Seanpm2001/Learn-Objective-CPP) as Objective-C++ is the language this project is dedicated to, because this project is about learning the Objective-C++ programming language. It only makes sense to make Objective-C++ the official language for this project.
+    	NSLog.println("I decided to make Objective-C++ the thirty-third project language file for this project (Seanpm2001/WacOS) as Objective-C++ is needed in some places where Objective-C can't go, and is another Apple language to be emulated and preserved in this project. It is getting its own project language file, starting here.");
+    	break;
+    	@end
+	}
+return projectLanguageFileOne();
+break;
+@end.
+}
+break;
+@end
+
+/* File info
+* File type: Objective-C++ source file (*.mm, *.m)
+* File version: 1 (2021, Thursday, December 23rd at 9:11 pm)
+* Line count (including blank lines and compiler line): 46
+*/
+// End of script
