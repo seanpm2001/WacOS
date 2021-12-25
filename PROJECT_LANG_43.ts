370f323861bc73e0f55950461057a5ae95834dc9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-24 17:00:03 -0800

    PROJECT_LANG_43.ts (V1)
    The current version (version 1) of the 43rd project language file for this proejct.

---
+++ PROJECT_LANG_43.ts
@@ -0,0 +1,24 @@
+// Start of script
+// I chose AssemblyScript as the fourty-third project language for this project (Seanpm2001/WacOS) as AssemblyScript is a good language for compiling to WebAssembly, and is a good language for the server/web portion of this project. It is being included for software diversity reasons, and will be used for some portions of the project. It is getting its own project language file, starting here. The language uses the TypeScript extension, as it is a dialect of TypeScript.
+class projectLanguageFileFortyThree() {
+  void main() {
+    // Writes the message to the console as well as popping up a dialog box, so that the message gets through one way or the other.
+    console.log ("Project language file 43");
+    alert("Project language file 43");
+    console.log ("For: Seanpm2001/WacOS/");
+    alert("For: Seanpm2001/WacOS");
+    console.log ("About:");
+    alert("About:");
+    console.log ("I chose AssemblyScript as the fourty-third project language for this project (Seanpm2001/WacOS) as AssemblyScript is a good language for compiling to WebAssembly, and is a good language for the server/web portion of this project. It is being included for software diversity reasons, and will be used for some portions of the project. It is getting its own project language file, starting here. The language uses the TypeScript extension, as it is a dialect of TypeScript.");
+    alert("I chose AssemblyScript as the fourty-third project language for this project (Seanpm2001/WacOS) as AssemblyScript is a good language for compiling to WebAssembly, and is a good language for the server/web portion of this project. It is being included for software diversity reasons, and will be used for some portions of the project. It is getting its own project language file, starting here. The language uses the TypeScript extension, as it is a dialect of TypeScript.");
+    break;
+	}
+// Note for project language files: The languages associated with SNU programming tools are not included as project language files, as there are too many to list.
+return main();
+break;
+/* File info
+* File version: 1 (2021, Friday, December 24th at 5:00 pm)
+* File type: AssemblyScript/TypeScript source file (*.ts *.tsx)
+* Line count (including blank lines and compiler line): 25
+*/
+// End of script
