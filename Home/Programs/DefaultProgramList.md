f4133b02e53b59f5e0b0db5ef513c511057cbc01
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-06 18:06:45 -0800

    Delete Home directory

--- Home/Programs/DefaultProgramList.md
+++
@@ -1,8 +0,0 @@
-# List of default programs on a Mac
-
-1. Finder
-2. Calculator
-3. Trash can
-4. Launchpad
-5. App store
-6. 
--- Home/Programs/ProgramFiles64/SwiftWiftIDELauncher.swift
+++
@@ -1,9 +0,0 @@
-// Start of script
-
-// This is the launcher file for the swift IDE (64 bit) it is currently incomplete.
-
-// Variables
-
-var swift_fileForm(".swift");
-
-// End of script
