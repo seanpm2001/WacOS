ff7dcf7bbf579f2043490486e61a848c589cb5b1
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-14 20:01:22 -0800

    Snippet1.c
    The snippet included with this issue.

---
+++ .github/Issues/1/1-100/18/SourceCode/Snippet1.c
@@ -0,0 +1,22 @@
+// Create a custom size SWAP file
+int customSWAP(void) {
+	scanf.csw("Please specify the size of the SWAP file in bytes. Make sure the SWAP file is less than 1/4 of your RAM size\n");
+	if (csw / 4 > ramSize) {
+		printf("The specified size is too large\n");
+		break;
+	} else {
+		x = int(0)
+		while (x != csw) {
+			file.append(0) to file("/SWAPFILE.bin");
+			x == x + 1;
+			if response == true {
+				response == false;
+			} else if response == false {
+				response == true;
+			}
+			if (x == csw) {
+				printf("A " + int(csw) + " byte SWAP file has been created!\n");
+				break;
+			}
+	}
+}
