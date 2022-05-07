e0117a90590c01179c6f817335a6f56f7cd56fe3
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-07 15:06:01 -0700

    Main.c [V1]
    Version 1 of the main C script for this directory set.,

---
+++ wOS/6/Main.c
@@ -0,0 +1,25 @@
+// Start of script
+// wOS 6 - A compatibility layer/recreation of iOS 6
+// This is the main thread that calls everything. It is not the entire subsystem.
+// License: GNU General Public License (V3.0)
+// This script is highly incomplete, and is not functional yet
+#include <stdio.h>
+#include <string.h>
+#include <Desktop/folders.h>
+#include <runtime.h>
+#include <aquaMarineUI.h>
+#include <skeuowie_i6.h>
+#include <touchGestures.h>
+int main() {
+	return main();
+	break;
+}
+return main()
+return 0;
+exit;
+/* File info
+File type: C source file (*.c)
+File version: 1 (2022, Saturday, May 7th at 3:06 pm PST)
+Line count (including blank lines and compiler line): 26
+*/
+// End of script
