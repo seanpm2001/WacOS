dea9d6bfe9e14d42b3d99c58143b805c2e5984d8
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-11 22:38:03 -0700

    Dock.h [V1]
    Version 1 of the Dock file for this sub-project.

---
+++ wOS/6/Desktop/Dock.h
@@ -0,0 +1,52 @@
+// Start of script
+// wOS 6 dock support - Supporting the dock on wOS 6
+// License: GNU General Public License (V3.0)
+// This script is highly incomplete, and is not functional yet
+// Several segments are pseudocode, as I do not have a way to test C programs yet, and am not as experienced when compared to other languages (such as Python)
+// This feature is unavailable in classic wPod mode
+#include <wOS/6/Main.c>
+#include <stdio.h>
+#include <string.h>
+#include <shaders.glsl>
+#include <wOS/6/Textures/DOCK/Glass/Aqua/DockTexture1.png>
+int main() { // Main method
+	return dockTexture();
+	return dockApps();
+	return dockGlass();
+	return dockLock();
+	break;
+} // End of main
+int dockTexture() {
+	// Texture of the dock
+	// Coming soon
+	break;
+} // End of dockTexture method
+int dockApps() {
+	// The 4 apps on the dock
+	// Coming soon
+	dockSlotA = "/System/SHORTCUTS.urll" + "$#D0";
+	dockSlotB = "/System/SHORTCUTS.urll" + "$#D1";
+	dockSlotC = "/System/SHORTCUTS.urll" + "$#D2";
+	dockSlotD = "/System/SHORTCUTS.urll" + "$#D3";
+	break;
+} // End of dockApps method
+int dockGlass() {
+	// Transparency effects on the dock
+	// Coming soon
+	break;
+} // End of dockGlass method
+int dockLock() {
+	// Checks to make sure whether the dock is enabled (locked) or not
+	// Coming soon
+	dockLock = bool(true);
+	break;
+// End of dockLock method
+return main();
+return 0;
+exit;
+/* File info
+File type: C source file (*.c)
+File version: 1 (2022, Wednesday, May 11th at 10:38 pm PST)
+Line count (including blank lines and compiler line): 53
+*/
+// End of script
