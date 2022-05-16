7f2508403a4d02ca16acf112087e42ddd9d6b83d
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-16 14:28:55 -0700

    2 sub-directories [41 files]
    2 sub-directories for this directory, containing 41 files in various file formats.

---
+++ Browser/OpenCyberdog/PROJECT_LANG_1.pas
@@ -0,0 +1,26 @@
+// Start of script
+
+program LanguageFileFileOne(output);
+begin
+    Write('Project language file 1')
+    Write('For: WacOS/OpenCyberdog')
+    Write('About:')
+    Write('I have decided to have the first project language file for this project (WacOS/OpenCyberdog) to be Pascal, as Pascal is a language used commonly in Classic Mac OS applications. Since the Savannah browser is designed to be more modern, it is written in C++. I chose not to write this project in C++, and I am also guessing that the original base project (Cyberdog) was written in Pascal, as it was a classic Mac OS application. If not, then that just makes this project more unique. Pascal is getting its own project language file, starting here.')
+    break
+    {No ";" is required after the last statement of a block -
+        adding one adds a "null statement" to the program, which is ignored by the compiler.}
+end.
+
+// Output:
+
+// Project language file 1
+// For: WacOS/OpenCyberdog
+// About:
+// I have decided to have the first project language file for this project (WacOS/OpenCyberdog) to be Pascal, as Pascal is a language used commonly in Classic Mac OS applications. Since the Savannah browser is designed to be more modern, it is written in C++. I chose not to write this project in C++, and I am also guessing that the original base project (Cyberdog) was written in Pascal, as it was a classic Mac OS application. If not, then that just makes this project more unique. Pascal is getting its own project language file, starting here
+
+// File info
+// File version: 1 (2022, Thursday, May 12th at 4:33 pm PST)
+// File type: Pascal source file (*.pas)
+// Line count (including blank lines and compiler line): 27
+
+// End of script
---
+++ Browser/OpenCyberdog/RepoData/Description/GitHub/OldVersions/English/US/1/1-100/README_V1.txt
@@ -0,0 +1,1 @@
+A separate repository for the archival and development of OpenCyberdog, an open source remake of Cyberdog. Written in Pascal, and part of the WacOS operating system project.
