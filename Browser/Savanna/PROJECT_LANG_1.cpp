bbd5afedc6da1f11e7b4c3da2ea9bc0d96699612
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-16 14:17:50 -0700

    1 sub-directory [39 files]
    1 sub-directory for this directory, containing 39 files in various file formats.

---
+++ Browser/Savanna/PROJECT_LANG_1.cpp
@@ -0,0 +1,40 @@
+// Start of script
+#include <iostream>
+// Please note: I am not very experienced with the C++ programming language yet, so this is just a demo script. It may not function as intended.
+
+// Start of source code
+
+class projectLanguageFileOne {
+
+int main() { // Main method
+    // Header
+    std::cout << "Project language file 1\n";
+    // Subheader
+    std::cout << "For: WacOS/Savannah\n";
+    // Neck
+    std::cout << "Reasoning:\n";
+    // Body
+    std::cout << "I have decided to have the first project language file for this project (WacOS/Savannah) to be C++, as on the opposite end, Pascal is a language used commonly in Classic Mac OS applications. Since the Savannah browser is designed to be more modern, it is written in C++ instead. It also matches the WebKit engine, which this project contains a fork of. This should allow for easy interoperability, and it is also a good language to write a web browser in. C++ is getting its own project language file, starting here.\n";
+    // return main(); /* Too recursive, removed */
+    break; // Not sure what this does, but I am using it as a stopping point.
+
+return main();
+break;
+return 0;
+
+}} // End of source code
+
+// Output:
+
+// Project language file 1
+// For: WacOS/Savannah
+// Reasoning:
+// I have decided to have the first project language file for this project (WacOS/Savannah) to be C++, as on the opposite end, Pascal is a language used commonly in Classic Mac OS applications. Since the Savannah browser is designed to be more modern, it is written in C++ instead. It also matches the WebKit engine, which this project contains a fork of. This should allow for easy interoperability, and it is also a good language to write a web browser in. C++ is getting its own project language file, starting here.
+
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx)
+* File version: 1 (2022, Thursday, March 12th at 4:42 pm PST)
+* Line count (including blank lines and compiler line): 41
+*/
+
+// End of script
