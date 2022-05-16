5d433de9057042a2b20c1933e334a6e73acbe704
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-16 14:31:55 -0700

    2 sub-directories [8 files]
    2 sub-directories for this directory, containing 8 files in Plain Text, Markdown, and C++ formats.

---
+++ Browser/Savanna/Savanna/!OldVersions/README/English/US/1/1-100/README_V1.md
@@ -0,0 +1,53 @@
+
+***
+
+# WacOS
+
+## Savannah browser
+
+Savannah (can be spelled either with or without the `h`) is a small project for a more open version of Safari, with better customization, better privacy, and better performance. It is going to be one of the 3 default browsers for WacOS.
+
+The other 2 default browsers are:
+
+- [x] OpenCyberdog
+- [x] Mozilla Firefox ESR
+
+## Engine
+
+Savannah will use an improved version of the WebKit engine. This is not to be confused with Chromium, as both WebKit and Chromium share source code.
+
+Some intended changes are:
+
+- [ ] Optional toggles for better CSS3, HTML5, and JavaScript compliance
+
+Intended features include:
+
+- [ ] A safari-like interface by default
+- [ ] As much customization power as Firefox
+- [ ] The ability to disable JavaScript
+- [ ] A completely open browser
+- [ ] Cross compatibility, allowing the testing of Safari webpages without owning a Macintosh or iPhone (via Safari mode)
+
+## Home repositories
+
+[Guesthouse repository](https://github.com/seanpm2001/WacOS_Savannah_Browser/)
+
+This is a guesthouse repository, and not a home repository, as development mainly stays on the main WacOS side. This is just the guesthouse that the project retreats to at times. If you are already in this repository, the link is likely recursive, and will reload the page.
+
+[Home repository](https://github.com/seanpm2001/WacOS/tree/WacOS-dev/Browser/Savanna/)
+
+This is the home repository. If you are already in this repository, the link is likely recursive, and will reload the page.
+
+***
+
+## File info
+
+**File type:** `Markdown document (*.md *.mkd *.mdown *.markdown)`
+
+**File version:** `1 (2022, Sunday, May 15th at 2:13 pm PST)`
+
+**Line count (including blank lines and compiler line):** `54`
+
+**Current article language:** `English (US)`
+
+***
---
+++ Browser/Savanna/Savanna/SafariMode/!OldVersions/MAIN/CPP/1/1-100/MAIN_V1.cpp
@@ -0,0 +1,269 @@
+// Start of script
+// This script is not yet functional
+// safari Mode for the Savanna(h) web browser, trying to simulate Safari in a more open way
+// License: GPL3 (GNU General Public License V3)
+// Note: I am not very experienced with C++, so this program likely won't function yet, and is mostly pseudocode
+// Standard C++ headers for Safari Mode
+#include <stdio.h> // Include the contents of the standard header 'stdio.h' (probably a file 'stdio.h').
+#include <vector> // Include the contents of the standard header 'vector' (probably a file 'vector.h').
+#include <chrono>
+#include <thread>
+// Program start
+class safariMode() {
+  // Safari mode
+  // Coming soon
+  std::cout << "Welcome to Safari Mode!\n";
+  break;
+  // Mac OS X 10.1 and earlier are not supported, as Safari wasn't available on those systems
+  int macOS_X_10_2_emulate() {
+    // Imitation of Mac OS X 10.2, Safari 1
+    // Coming soon
+    break;
+  }
+  int macOS_X_10_3_emulate() {
+    // Imitation of Mac OS X 10.3, Safari 2
+    // Coming soon
+    break;
+  }
+  int macOS_X_10_4_emulate() {
+    // Imitation of Mac OS X 10.4, Safari 3
+    // Coming soon
+    break;
+  }
+  int macOS_X_10_5_emulate() {
+    // Imitation of Mac OS X 10.5, Safari 4
+    // Coming soon
+    break;
+  }
+  int macOS_X_10_6_emulate() {
+    // Imitation of Mac OS X 10.6, Safari 5
+    // Coming soon
+    break;
+  }
+  int OS_X_10_7_emulate() {
+    // Imitation of OS X 10.7, Safari 7
+    // Coming soon
+    break;
+  }
+  int OS_X_10_8_emulate() {
+    // Imitation of OS X 10.8, Safari 8
+    // Coming soon
+    break;
+  }
+  int OS_X_10_9_emulate() {
+    // Imitation of OS X 10.9, Safari 9
+    // Coming soon
+    break;
+  }
+  int OS_X_10_10_emulate() {
+    // Imitation of OS X 10.10, Safari 10
+    // Coming soon
+    break;
+  }
+  int OS_X_10_11_emulate() {
+    // Imitation of OS X 10.11, Safari 11
+    // Coming soon
+    break;
+  }
+  int macOS_10_12_emulate() {
+    // Imitation of MacOS 10.12, Safari 12
+    // Coming soon
+    break;
+  }
+  int macOS_10_13_emulate() {
+    // Imitation of MacOS 10.13, Safari 13
+    // Coming soon
+    break;
+  }
+  int macOS_10_14_emulate() {
+    // Imitation of MacOS 10.14, Safari 14
+    // Coming soon
+    break;
+  }
+  int macOS_10_15_emulate() {
+    // Imitation of MacOS 10.15, Safari 14
+    // Coming soon
+    break;
+  }
+  int macOS_11_emulate() {
+    // Imitation of MacOS 11, Safari 14
+    // Coming soon
+    break;
+  }
+  int macOS_12_emulate() {
+    // Imitation of macOS 12, Safari 15  
+    // Coming soon
+    break;
+  }
+  int iOS_6_emulate() {
+    // Imitation of iOS 6, Safari 6
+    // Coming soon
+    break;
+  }
+  int iOS_7_emulate() {
+    // Imitation of iOS 7, Safari 7
+    // Coming soon
+    break; 
+  }
+  int iOS_8_emulate() {
+    // Imitation of iOS 8, Safari 8
+    // Coming soon
+    break; 
+  }
+  int iOS_9_emulate() {
+    // Imitation of iOS 9, Safari 9
+    // Coming soon
+    break; 
+  }
+  int iOS_10_emulate() {
+    // Imitation of iOS 10, Safari 10
+    // Coming soon
+    break; 
+  }
+  int iOS_11_emulate() {
+    // Imitation of iOS 11, Safari 11
+    // Coming soon
+    break; 
+  }
+  int iOS_12_emulate() {
+    // Imitation of iOS 12, Safari 12
+    // Coming soon
+    break; 
+  }
+  int iOS_13_emulate() {
+    // Imitation of iOS 13, Safari 13
+    // Coming soon
+    break; 
+  }
+  int iOS_14_emulate() {
+    // Imitation of iOS 14, Safari 14
+    // Coming soon
+    break; 
+  }
+  int iOS_15_emulate() {
+    // Imitation of iOS 15, Safari 15
+    // Coming soon
+    break; 
+  }
+  int iPadOS_13_emulate() {
+    // Imitation of iPadOS 13, Safari 13
+    // Coming soon
+    break; 
+  }
+  int iPadOS_14_emulate() {
+    // Imitation of iPadOS 14, Safari 14
+    // Coming soon
+    break; 
+  }
+  int iPadOS_15_emulate() {
+    // Imitation of iPadOS 15, Safari 15
+    // Coming soon
+    break; 
+  }
+  int versionToggle() {
+  verUUID == char("W6\n");
+  if verUUID == "W1\n" {
+  	return macOS_10_2_emulate();
+  	break;
+  } else if verUUID == "W2\n" {
+  	return macOS_10_3_emulate();
+  	break;
+  } else if verUUID == "W3\n" {
+  	return macOS_10_4_emulate();
+  	break;
+  } else if verUUID == "W4\n" {
+  	return macOS_10_5_emulate();
+  	break;
+  } else if verUUID == "W5\n" {
+  	return macOS_10_6_emulate();
+  	break;
+  } else if verUUID == "W6\n" {
+  	return OS_X_10_7_emulate();
+  	break;
+  } else if verUUID == "W7\n" {
+  	return OS_X_10_8_emulate();
+  	break;
+  } else if verUUID == "W8\n" {
+  	return OS_X_10_9_emulate();
+  	break;
+  } else if verUUID == "W9\n" {
+  	return OS_X_10_10_emulate();
+  	break;
+  } else if verUUID == "W10\n" {
+  	return OS_X_10_11_emulate();
+  	break;
+  } else if verUUID == "W11\n" {
+  	return macOS_10_12_emulate();
+  	break;
+  } else if verUUID == "W12\n" {
+  	return macOS_10_13_emulate();
+  	break;
+  } else if verUUID == "W13\n" {
+  	return macOS_10_14_emulate();
+  	break;
+  } else if verUUID == "W14\n" {
+  	return macOS_10_15_emulate();
+  	break;
+  } else if verUUID == "W15\n" {
+  	return macOS_11_emulate();
+  	break;
+  } else if verUUID == "W16\n" {
+  	return macOS_12_emulate();
+  	break;
+  }
+  // Type of system divider
+  if verUUID == "WI1\n" {
+  	return iOS_6_emulate();
+  	break;
+  } else if verUUID == "WI2\n" {
+  	return iOS_7_emulate();
+  	break;
+  } else if verUUID == "WI3\n" {
+  	return iOS_8_emulate();
+  	break;
+  } else if verUUID == "WI4\n" {
+  	return iOS_9_emulate();
+  	break;
+  } else if verUUID == "WI5\n" {
+  	return iOS_10_emulate();
+  	break;
+  } else if verUUID == "WI6\n" {
+  	return iOS_11_emulate();
+  	break;
+  } else if verUUID == "WI7\n" {
+  	return iOS_12_emulate();
+  	break;
+  } else if verUUID == "WI8\n" {
+  	return iOS_13_emulate();
+  	break;
+  } else if verUUID == "WI9\n" {
+  	return iOS_14_emulate();
+  	break;
+  } else if verUUID == "WI10\n" {
+  	return iOS_15_emulate();
+  	break;
+  }
+  // Type of system divider
+  if verUUID == "WPA1\n" {
+  	return iPadOS_13_emulate();
+  	break;
+  } else if verUUID == "WPA2\n" {
+  	return iPadOS_14_emulate();
+  	break;
+  } else if verUUID == "WPA3\n" {
+  	return iPadOS_15_emulate();
+  	break;
+  }
+}
+break;
+return 0;
+/* Todo
+Find a way to make the starter amount not reset each time
+*/
+break;
+/* File info
+* File type: C++ program file (*.C *.cpp *.cxx *.c++, .cc, .h, .H, .hh, .hpp, .hxx, .h++)
+* File version: 1 (2022, Sunday, May 15th at 2:51 pm PST)
+* Line count (including blank lines and compiler line): 270
+*/
+// End of script
