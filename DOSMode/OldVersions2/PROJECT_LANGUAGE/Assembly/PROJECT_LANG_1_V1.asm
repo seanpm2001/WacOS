5ff3612f78caf17f1d78f0942e7ff2337811aa3e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-16 15:18:46 -0700

    3 sub-directories [52 files]
    3 sub-directories for this directory, containing 52 files in various file formats.

---
+++ DOSMode/OldVersions2/PROJECT_LANGUAGE/Assembly/PROJECT_LANG_1_V1.asm
@@ -0,0 +1,13 @@
+; Start of script
+
+; Project language file 1
+; For: WacOS/DOSMode
+; About:
+; I have decided to make Assembly the first project language for this project (WacOS/DOSMode) because Assembly is a very low level language that can access the required sections of memory needed for this project. Since this is being based on a project with an extremely light operating system, Assembly may be the only language used. Pascal for sure won't be used, but there is a chance I might include C in the future if needed. Assembly was also the first language of the 2 original subprojects to this project (4-Bit_DOSMode & 8-Bit_DOSMode) It is getting its own project language file, starting here.
+
+; File info
+; File version: 1 (2022, Sunday, May 15th at 7:00 pm PST)
+; File type: Assembly source file (*.asm)
+; Line count (including blank lines and compiler line): 14
+
+; End of script
---
+++ DOSMode/RepoData/Description/GitHub/README.txt
@@ -0,0 +1,1 @@
+A separate repository for DOS Mode (WacOS) for the WacOS operating system project, containing both the 4 bit and 8 bit LIBraries.
