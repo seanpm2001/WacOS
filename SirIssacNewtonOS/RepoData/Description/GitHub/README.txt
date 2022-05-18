a721c7d5c7f59e799cafd2f9a6b705efb94dcd20
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-18 12:47:03 -0700

    3 sub-direcotries [41 files]
    3 sub-directories for this directory, containing 41 files in various file formats.

---
+++ SirIssacNewtonOS/RepoData/Description/GitHub/README.txt
@@ -0,0 +1,1 @@
+SirIsaacNewtonOS is a open source recreation and alternate to Apples NewtonOS. It is part of the WacOS Operating System project.
---
+++ SirIssacNewtonOS/SirIssacNewtonOS/MAIN.cpp
@@ -0,0 +1,30 @@
+// Start of script
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+// Main script for Sir Isaac Newton OS
+// An open source recreation of NewtonOS
+// Please note that I am not experienced with C++ so this file may not be functional and may come off as bad code
+class isaac() {
+  int mem() {
+    std::cout >> "Sir Isaac OS is an 8x16 bit operating system designed to run with 128 kilobytes of memory and up to a gigabyte of storage";
+    raMax = int("128000);
+    storageMax = int("1000000000");
+    modelProcess = str("8 bit/16 bit computer operating system");
+    break;
+  }
+  int isaccScript() {
+    return ("/WacOS/SirIsaacNewTonScript/");
+    break;
+  }
+  return mem();
+  return isaacScript();
+  break;
+  exit;
+}
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx)
+* File version: 1 (Monday, Apugust 30th 2021 at 4:13 pm)
+* Line count (including blank lines and compiler line): 31
+*/
+// End of script
