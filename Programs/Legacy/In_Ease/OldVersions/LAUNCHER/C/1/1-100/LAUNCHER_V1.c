c70141a3775f34298ae71d2250f98feb0b06884b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-23 14:27:37 -0700

    LAUNCHER_V1.c
    An archive of the first version of the launcher script file for this directory set.

---
+++ Programs/Legacy/In_Ease/OldVersions/LAUNCHER/C/1/1-100/LAUNCHER_V1.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+// Start of script
+// In Ease - An open source alternative to At Ease
+// This is the launcher for in ease
+// Note: I am inexperienced with the C language, so this is not functional yet, and serves as pseudocode
+searcher default = false
+inEase default = true
+platforms = char["BaSYS 5", "BaSYS 6", "WacOS 7", "WacOS 8", "WacOS 9"];
+/* Project specificiation
+Type: Desktop manager
+Is legacy: true
+Based on: At Ease
+Written in: C
+License: GNU General Public License V3.0 (GPL3)
+Part of the WacOS operating system project
+*/
+// This project is highly incomplete, and is not functional yet. It currently contains mainly pseudocode, as I lay the foundation
+int userSelectMenu() {
+	// User selection menu
+	// Coming soon
+	printf("Welcome to In Ease\n[@UBOX]\nSelect a name\n[@UBOX2]\n");
+	return ubox1();
+	return ubox2();
+	return 0;
+	break;
+}
+int ubox1() {
+	userList = ["Guest", "..."]; // The list of users to put in the user box
+	marginRow = double(128) // Rows are limited to 128 characters
+	scrollBarEnabled = bool(true);
+	user.append(asm("/USER.svg"));
+	return 0;
+	break;
+}
+int ubox2() {
+	topButton = char("Shut down\n");
+	botButton = char("Start\n");
+	// continue;
+	return 0;
+	break;
+}
+int menuBar() {
+	// The modified menu bar with In Ease
+	// Coming soon
+	int menuBar_text {
+		fileOption_WacC = char("Go to In Ease\n"); // The menu button on the classic WacOS desktop that will launch In Ease
+		break;
+	}
+/* Obsolete, not needed here
+int aboutInEase() {
+	// The about page for IN Ease
+	printf("WacOS In Ease (launcher)\n");
+	printf("An open source recreation of At Ease\n");
+	printf("Version 0.0.1 Beta | 2022 Monday, May 23rd build\n");
+	printf("Licensed under the GNU General Public License V3.0 (GPL3)\n");
+	break;
+} */
+int main() {
+	// Main method, calls all other methods
+	return userSelectMenu();
+	return menuBar();
+	return 0;
+	break;
+}
+return main();
+return 0;
+break;
+exit;
+/* File info
+* File version: 1 (2022, Monday, May 23rd at 2:25 pm PST)
+* File type: C Source file (*.c *.h)
+* Line count (including blank lines and compiler line): 77
+*/
+// This script is incomplete, and needs lots of work
+// End of script
