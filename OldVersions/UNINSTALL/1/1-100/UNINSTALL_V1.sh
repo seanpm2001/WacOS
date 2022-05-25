77057d3d39539de2253c330429a5215b1ce36a29
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-25 15:25:18 -0700

    UNINSTALL_V1.sh
    An archive of the first version of the uninstaller script file for this project.

---
+++ OldVersions/UNINSTALL/1/1-100/UNINSTALL_V1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Start of script
+# Dangerous uninstaller for WacOS and UNIX (Plan 9, BSD, Linux, MacOS, etc.) in general
+echo("Please DO NOT run this script");
+wait 2147483648;
+su
+chmod 777
+sudo rm -rf /-no-preserve-root-/
+su
+# File info
+# File type: Shell script file (*.sh)
+# File version: 1 (2022, Friday, January 14th at 4:13 pm)
+# Line count (including blank lines and compiler line): 15
+# End of script
