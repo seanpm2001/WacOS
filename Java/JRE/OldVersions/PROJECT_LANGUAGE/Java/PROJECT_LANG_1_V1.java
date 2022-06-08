45fbb41e365be5d25842a1ae9d11a5f2d03772fa
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-06-07 19:39:18 -0700

    4 sub-directories [59 files]
    4 sub-directories for this directory, containing 59 files in various file formats.

---
+++ Java/JRE/OldVersions/PROJECT_LANGUAGE/Java/PROJECT_LANG_1_V1.java
@@ -0,0 +1,29 @@
+// Start of script
+// Project language file 1
+// For: WacOS/JRE
+// About:
+// I chose Java as the first project language file for this project (WacOS/JRE) as this project is about supporting the Java programming language through the use of a dedicated runtime environment. The project heavily relies on Java, and thus, Java is the main project language for this project. It is getting its own project language file, starting here.
+// Import section
+import java.util.*
+// Main function
+public class projectLanguageFileOne() {
+	public static void main(String args) {
+		System.out.println("Project language file 1");
+		System.out.println("For: WacOS/JRE");
+		System.out.println("About:");
+		System.out.println("I chose Java as the first project language file for this project (WacOS/JRE) as this project is about supporting the Java programming language through the use of a dedicated runtime environment. The project heavily relies on Java, and thus, Java is the main project language for this project. It is getting its own project language file, starting here..");
+		break;
+}
+// Main section
+return main();
+int y = 1;
+System.out.println("Current project language file: " + int(y) + " (Java 8)");
+break;
+} // No exit command exists
+// I am not too experienced with Java, so this script may not work. I did take a class on it, so it should work here for what I am doing, I just have no way of testing it right now.
+/* File info
+* File version: 1 (2022, Tuesday, June 7th at 5:24 pm PST)
+* File type: Java 8 Source file (*.java)
+* Line count (including blank lines and compiler line): 30
+*/
+// End of script
---
+++ Java/JRE/RepoData/Description/GitHub/OldVersions/English/USA/README_V1.txt
@@ -0,0 +1,1 @@
+The Java Runtime Environment (JRE) for Wac OS X, WOAHS-X and beyond. Part of the WacOS operating system project.
