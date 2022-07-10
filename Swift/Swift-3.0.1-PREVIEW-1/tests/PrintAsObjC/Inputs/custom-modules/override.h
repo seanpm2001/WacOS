8ed505878f62a3c60a71d1c9307a4d7b17da04e3
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-09 18:27:53 -0700

    1 sub-directory [50 files]
    1 sub-directory for this directory, containing 50 files in various file formats.

---
+++ Swift/Swift-3.0.1-PREVIEW-1/tests/PrintAsObjC/Inputs/custom-modules/override.h
@@ -0,0 +1,17 @@
+@import Foundation;
+
+@interface Base : NSObject
+- (NSUInteger)foo;
+- (NSUInteger)foo:(NSUInteger)ignored;
+- (NSUInteger)foo:(NSUInteger)x y:(NSUInteger)y;
+
+@property(readonly,getter=getProp) NSUInteger prop;
+
+- (id)objectAtIndexedSubscript:(NSUInteger)idx;
+
+- (void)callback:(NSUInteger(^)(void))block;
+
+- (BOOL)doThingAndReturnError:(NSError **)error;
+- (BOOL)doAnotherThingWithError:(NSError **)error;
+
+@end
