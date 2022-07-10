e9ecaf3b10a4f7ee49908746b2aa5c6e752dc48b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-10 15:17:46 -0700

    12 sub-directories [90 files]
    12 sub-directories for this directory, containing 90 files in various file formats.

---
+++ Swift/Swift-3.0.1-PREVIEW-1/tests/Driver/Dependencies/Inputs/update-dependencies-bad.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# update-dependencies-bad.py - Fails on bad.swift -*- python -*-
+#
+# This source file is part of the Swift.org open source project
+#
+# Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+# Licensed under Apache License v2.0 with Runtime Library Exception
+#
+# See http://swift.org/LICENSE.txt for license information
+# See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+#
+# ----------------------------------------------------------------------------
+#
+# Fails if the input file is named "bad.swift" or "crash.swift"; otherwise
+# dispatches to update-dependencies.py. "crash.swift" gives an exit code
+# other than 1.
+#
+# ----------------------------------------------------------------------------
+
+from __future__ import print_function
+
+import os
+import shutil
+import sys
+
+assert sys.argv[1] == '-frontend'
+
+primaryFile = sys.argv[sys.argv.index('-primary-file') + 1]
+
+if (os.path.basename(primaryFile) == 'bad.swift' or
+        os.path.basename(primaryFile) == 'crash.swift'):
+    print("Handled", os.path.basename(primaryFile))
+
+    # Replace the dependencies file with the input file.
+    try:
+        depsFile = sys.argv[sys.argv.index(
+            '-emit-reference-dependencies-path') + 1]
+        shutil.copyfile(primaryFile, depsFile)
+    except ValueError:
+        pass
+
+    if os.path.basename(primaryFile) == 'bad.swift':
+        sys.exit(1)
+    else:
+        sys.exit(129)
+
+dir = os.path.dirname(os.path.abspath(__file__))
+execfile(os.path.join(dir, "update-dependencies.py"))
