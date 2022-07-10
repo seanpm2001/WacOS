e0e5c5b1c7c9b772e3334ccdd5dcf44cfe05da68
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-09 17:01:01 -0700

    Batch 11 - 100 files
    The 11th batch of files for this directory, containing 100 files in various file formats.

---
+++ Pascal/Latest/Pascal-P5/standard_tests/iso7185prt0028.pas
@@ -0,0 +1,12 @@
+{
+
+PRT test 28: Missing ident in type
+
+}
+
+program iso7185prt0028;
+
+type  = char;
+
+begin
+end.
---
+++ Pascal/Latest/Pascal-P5/standard_tests/iso7185prt0045.pas
@@ -0,0 +1,15 @@
+{
+
+PRT test 45: Missing block
+
+}
+
+program iso7185prt0045;
+
+procedure x; ;
+
+begin
+
+   x
+
+end.
