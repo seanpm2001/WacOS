c6fcd4dff64a381f9ca683ad7a643bfc10c79903
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-10 15:22:54 -0700

    2 sub-directories [100 files]
    2 sub-directories for this directory, containing 100 files in various file formats.

---
+++ Swift/Swift-3.0.1-PREVIEW-1/tests/IDE/Inputs/print_clang_header/header-to-print.h
@@ -0,0 +1,31 @@
+#import <Foundation.h>
+@import Dispatch;
+#include "other-header.h"
+
+#define MACRO_GOT_UNDEFINED 1
+#undef MACRO_GOT_UNDEFINED
+#define MY_MACRO 1
+
+void doSomethingInHead(int arg);
+
+@interface BaseInHead
+- (void)doIt:(int)arg;
+@end
+
+/// Awesome name.
+@interface SameName
+@end
+@protocol SameName
+@end
+
+@interface BaseInHead(SomeCategory)
+-(void)doItInCategory;
+@end
+
+@protocol Superproto
+-(void)lala;
+@end
+
+@class Cake;
+struct Arkham;
+@protocol Soul;
