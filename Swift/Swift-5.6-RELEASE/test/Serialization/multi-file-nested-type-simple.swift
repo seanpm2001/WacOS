ec66e5e510285b68b5c9da1e436bd8ac6d8449c9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-11 16:23:03 -0700

    91 files
    91 files for this directory, in Swift, and Sil formats.

---
+++ Swift/Swift-5.6-RELEASE/test/Serialization/multi-file-nested-type-simple.swift
@@ -0,0 +1,18 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-swift-frontend -emit-module -module-name Multi -o %t/multi-file.swiftmodule -primary-file %s %S/Inputs/multi-file-nested-types.swift
+// RUN: %target-swift-frontend -emit-module -module-name Multi -o %t/multi-file-2.swiftmodule %s -primary-file %S/Inputs/multi-file-nested-types.swift
+
+// RUN: %target-swift-frontend -emit-module -module-name Multi %t/multi-file.swiftmodule %t/multi-file-2.swiftmodule -o %t -print-stats 2>&1 | %FileCheck %s
+
+// Switch the order of the files.
+// RUN: %target-swift-frontend -emit-module -module-name Multi %t/multi-file-2.swiftmodule %t/multi-file.swiftmodule -o %t -print-stats 2>&1 | %FileCheck %s
+
+// REQUIRES: asserts
+
+// CHECK: 4 Serialization - # of nested types resolved without full lookup
+
+public func useTypes(
+  _: Outer.Inner,
+  _: Outer.InnerAlias,
+  _: OuterClass.Inner,
+  _: OuterClass.InnerAlias) {}
