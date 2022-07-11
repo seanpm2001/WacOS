ce6f40ad80b43cb635dc457404ac8a0ee0e4f9ed
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-11 16:25:17 -0700

    15 sub-directories [100 files]
    15 sub-directories for this directory, along with 100 files in various file formats.

---
+++ Swift/Swift-5.6-RELEASE/test/Serialization/Inputs/binary_sub.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+(_, old, new) = sys.argv
+assert(len(old) == len(new))
+
+if sys.version_info[0] < 3:
+    data = sys.stdin.read()
+    sys.stdout.write(data.replace(old, new))
+else:
+    data = sys.stdin.buffer.read()
+    sys.stdout.buffer.write(data.replace(old.encode('utf-8'), new.encode('utf-8')))
