0954cbaec1e64d03c9dc3d9646f2b37f5642ada2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-11 15:55:04 -0700

    Batch 5 - 100 files
    The 5th batch of files [out of 8] for this directory, containing 100 files in Swift, and SIL formats.

---
+++ Swift/Swift-5.6-RELEASE/test/SILOptimizer/no_size_specialization.swift
@@ -0,0 +1,21 @@
+// RUN: %target-swift-frontend  %s -O -emit-sil | %FileCheck %s -check-prefix=CHECK-O
+// RUN: %target-swift-frontend  %s -Osize -emit-sil | %FileCheck %s -check-prefix=CHECK-OSIZE
+
+@_semantics("optimize.sil.specialize.generic.size.never")
+func foo<T>(_ t: T) -> T {
+  return t
+}
+
+// CHECK-O-LABEL: sil @{{.*}}test
+// CHECK-O: %[[LITERAL:.+]] = integer_literal $Builtin.Int{{[0-9]+}}, 27
+// CHECK-O: %[[STRUCT:.+]] = struct $Int (%[[LITERAL]] : $Builtin.Int{{[0-9]+}})
+// CHECK-O: return %[[STRUCT]]
+
+// CHECK-OSIZE-LABEL: sil {{.*}} @{{.*}}foo
+
+// CHECK-OSIZE-LABEL: sil @{{.*}}test
+// CHECK-OSIZE: function_ref {{.*}}foo
+// CHECK-OSIZE: apply
+public func test() -> Int {
+  return foo(27)
+}
