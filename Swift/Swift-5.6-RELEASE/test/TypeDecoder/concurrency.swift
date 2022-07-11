03a97d35ff8316e1fbe06953f58f19480b8f7f00
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-10 20:44:58 -0700

    1 sub-directory [21 files]
    1 sub-directory for this directory, containing 21 files in various file formats.

---
+++ Swift/Swift-5.6-RELEASE/test/TypeDecoder/concurrency.swift
@@ -0,0 +1,30 @@
+// RUN: %empty-directory(%t)
+
+// RUN: %target-build-swift -emit-executable %s -g -o %t/concurrency -emit-module 
+
+// RUN: sed -ne '/\/\/ *DEMANGLE-TYPE: /s/\/\/ *DEMANGLE-TYPE: *//p' < %s > %t/input
+// RUN: %lldb-moduleimport-test-with-sdk %t/concurrency -type-from-mangled=%t/input | %FileCheck %s --check-prefix=CHECK-TYPE
+
+// REQUIRES: concurrency
+
+func blackHole(_: Any...) {}
+
+public var lookAtMeeee: [(Int) async -> Void] = []
+
+func foo() {
+  do {
+    let x1 = [(Int) async -> Void]()
+    let x2 = [(Int) async throws -> Void]()
+
+    blackHole(x1, x2)
+  }
+}
+
+// DEMANGLE-TYPE: $sSayySiYacG
+// CHECK-TYPE: Array<(Int) async -> ()>
+
+// DEMANGLE-TYPE: $sSayySiYaKcG
+// CHECK-TYPE: Array<(Int) async throws -> ()>
+
+// DEMANGLE-TYPE: $sIegH_D
+// CHECK-TYPE: @async @callee_guaranteed () -> ()
