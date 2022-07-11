282c20a85ea0a0463ad65371971e774d818ca679
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-10 20:34:26 -0700

    1 sub-directory [88 files]
    1 sub-directory for this directory, containing 88 files in various file formats.

---
+++ Swift/Swift-5.6-RELEASE/test/PrintAsObjC/accessibility.swift
@@ -0,0 +1,59 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-swift-frontend -parse-as-library %s -typecheck -emit-objc-header-path %t/accessibility.h -disable-objc-attr-requires-foundation-module
+// RUN: %FileCheck -check-prefix=CHECK -check-prefix=CHECK-PUBLIC %s < %t/accessibility.h
+// RUN: %check-in-clang %t/accessibility.h
+
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) %s -typecheck -emit-objc-header-path %t/accessibility-internal.h -disable-objc-attr-requires-foundation-module
+// RUN: %FileCheck -check-prefix=CHECK -check-prefix=CHECK-INTERNAL %s < %t/accessibility-internal.h
+// RUN: %check-in-clang %t/accessibility-internal.h
+
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -parse-as-library %s -typecheck -import-objc-header %S/../Inputs/empty.h -emit-objc-header-path %t/accessibility-imported-header.h -disable-objc-attr-requires-foundation-module
+// RUN: %FileCheck -check-prefix=CHECK -check-prefix=CHECK-INTERNAL %s < %t/accessibility-imported-header.h
+// RUN: %check-in-clang %t/accessibility-imported-header.h
+
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -parse-as-library %s -typecheck -DMAIN -emit-objc-header-path %t/accessibility-main.h -disable-objc-attr-requires-foundation-module
+// RUN: %FileCheck -check-prefix=CHECK -check-prefix=CHECK-INTERNAL %s < %t/accessibility-main.h
+// RUN: %check-in-clang %t/accessibility-main.h
+
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -parse-as-library %s -typecheck -application-extension -emit-objc-header-path %t/accessibility-appext.h -disable-objc-attr-requires-foundation-module
+// RUN: %FileCheck -check-prefix=CHECK -check-prefix=CHECK-INTERNAL %s < %t/accessibility-appext.h
+// RUN: %check-in-clang %t/accessibility-appext.h
+
+// REQUIRES: objc_interop
+
+// CHECK: #ifndef ACCESSIBILITY_SWIFT_H
+// CHECK-NEXT: #define ACCESSIBILITY_SWIFT_H
+
+// CHECK-LABEL: @interface A_Public{{$}}
+// CHECK-INTERNAL-NEXT: init
+// CHECK-NEXT: @end
+@objc @objcMembers public class A_Public {}
+
+// CHECK-PUBLIC-NOT: B_Internal
+// CHECK-INTERNAL-LABEL: @interface B_Internal{{$}}
+// CHECK-INTERNAL-NEXT: init
+// CHECK-INTERNAL-NEXT: @end
+@objc @objcMembers internal class B_Internal {}
+
+// CHECK-NOT: C_Private
+@objc @objcMembers private class C_Private {}
+
+
+#if MAIN
+#if canImport(AppKit)
+import AppKit
+
+@NSApplicationMain
+@objc class AppDelegate : NSApplicationDelegate {}
+
+#elseif canImport(UIKit)
+import UIKit
+
+@UIApplicationMain
+@objc class AppDelegate : NSObject, UIApplicationDelegate {}
+
+#else
+// Uh oh, this test depends on having an app delegate.
+#error("Unsupported platform")
+#endif
+#endif
