2f6691427d5c1344de245ac1221fe747742472f0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-10 20:09:41 -0700

    7 sub-directories [63 files]
    7 sub-directories for this directory, containing 63 files in various file formats.

---
+++ Swift/Swift-5.6-RELEASE/test/APINotes/blocks.swift
@@ -0,0 +1,16 @@
+// RUN: %target-build-swift -Xfrontend %clang-importer-sdk %s -emit-ir
+
+// REQUIRES: objc_interop
+
+import UIKit
+
+class MyView: UIView {
+  func foo() {
+    UIView.animate(withDuration: 1, delay: 1, options: UIViewAnimationOptions.layoutSubviews,
+      animations: { print("animating") },
+      completion: { (finished: Bool) -> Void in print("here we are") });
+    UIView.animate(withDuration: 1, delay: 1, options: UIViewAnimationOptions.layoutSubviews,
+      animations: { print("animating") },
+      completion: nil);
+  }
+}
