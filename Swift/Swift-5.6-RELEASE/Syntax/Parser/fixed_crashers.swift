91dc34976d8c2a0a57a3e033a4f1404e07e89b66
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-10 20:22:38 -0700

    2 sub-directories [56 files]
    2 sub-directories for this directory, containing 56 files in various file formats.

---
+++ Swift/Swift-5.6-RELEASE/Syntax/Parser/fixed_crashers.swift
@@ -0,0 +1,11 @@
+// REQUIRES: syntax_parser_lib
+// RUN: %swift-syntax-parser-test %s -dump-tree | %FileCheck %s
+
+let x: a[i] & b
+// CHECK: |x|
+
+let x2: a & b[1]
+// CHECK: |x2|
+
+let x3: (A -> B)
+// CHECK: |x3|
