f9afbc36c201d9e2d4d9d3ea6d27f38fd76b595e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-10 20:31:58 -0700

    Delete Swift/Swift-5.6-RELEASE/PlaygroundTransform directory

--- Swift/Swift-5.6-RELEASE/PlaygroundTransform/import_error.swift
+++
@@ -1,5 +0,0 @@
-// RUN: %empty-directory(%t)
-// RUN: cp %s %t/main.swift
-// RUN: %target-swift-frontend -typecheck -playground %t/main.swift -verify
-
-var $a = 2 // expected-error {{cannot declare entity named '$a'}}
