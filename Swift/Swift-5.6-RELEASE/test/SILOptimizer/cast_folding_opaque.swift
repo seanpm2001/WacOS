96ff609d35f1e9f1c15510783eda1f11b8ec0d2c
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-11 15:48:26 -0700

    Batch 1 - 100 files
    The 1st batch of files [out of 8] for this directory, containing 100 files in Swift, GYB, and SIL formats.

---
+++ Swift/Swift-5.6-RELEASE/test/SILOptimizer/cast_folding_opaque.swift
@@ -0,0 +1,12 @@
+// RUN: %target-swift-frontend -enable-library-evolution -disable-availability-checking -O -emit-sil %s %S/Inputs/cast_folding_opaque_other.swift -module-name cast_folding_opaque
+// RUN: %target-swift-frontend -enable-library-evolution -disable-availability-checking -Onone -emit-sil -primary-file %s %S/Inputs/cast_folding_opaque_other.swift -module-name cast_folding_opaque | %FileCheck %s
+
+// CHECK-LABEL: sil @$s19cast_folding_opaque23testCastOpaqueArchetypeAA10UnderlyingVyF
+// CHECK:   [[O:%.*]] = alloc_stack $@_opaqueReturnTypeOf("$s19cast_folding_opaque12returnOpaqueQryF", 0)
+// CHECK:   [[F:%.*]] = function_ref @$s19cast_folding_opaque12returnOpaqueQryF
+// CHECK:   apply [[F]]([[O]])
+// CHECK:   unconditional_checked_cast_addr @_opaqueReturnTypeOf{{.*}}in [[O]] : $*@_opaqueReturnTypeOf{{.*}}to Underlying in %0 : $*Underlying
+@inlinable
+public func testCastOpaqueArchetype() -> Underlying {
+  return returnOpaque() as! Underlying
+}
