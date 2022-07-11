25152ba859e1708755adbe85cf137eb8e2cab9b9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-11 16:45:20 -0700

    1 sub-directory [76 files]
    1 sub-directory for this directory, along with 76 files in various file formats.

---
+++ Swift/Swift-5.6-RELEASE/test/Interop/Cxx/templates/Inputs/mangling.h
@@ -0,0 +1,13 @@
+#ifndef TEST_INTEROP_CXX_TEMPLATES_INPUTS_MANGLING_H
+#define TEST_INTEROP_CXX_TEMPLATES_INPUTS_MANGLING_H
+
+template<typename T>
+struct MagicWrapper {};
+
+struct IntWrapper {};
+
+typedef MagicWrapper<int> WrappedMagicInt;
+typedef MagicWrapper<bool> WrappedMagicBool;
+
+
+#endif // TEST_INTEROP_CXX_TEMPLATES_INPUTS_MANGLING_H
