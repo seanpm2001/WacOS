3cce88910902172fca78fac4f049909fe342e07f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-10 19:25:37 -0700

    14 sub-directories [99 files]
    14 sub-directories for this directory, containing 99 files in various file formats.

---
+++ Swift/Swift-5.6-RELEASE/unittests/AST/TestContext.cpp
@@ -0,0 +1,67 @@
+//===--- TestContext.cpp - Helper for setting up ASTContexts --------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#include "TestContext.h"
+#include "swift/AST/GenericParamList.h"
+#include "swift/AST/Module.h"
+#include "swift/AST/ParseRequests.h"
+#include "swift/Strings.h"
+#include "swift/Subsystems.h"
+
+using namespace swift;
+using namespace swift::unittest;
+
+static Decl *createOptionalType(ASTContext &ctx, SourceFile *fileForLookups,
+                                Identifier name) {
+  auto wrapped = new (ctx) GenericTypeParamDecl(fileForLookups,
+                                                ctx.getIdentifier("Wrapped"),
+                                                SourceLoc(),
+                                                /*type sequence*/ false,
+                                                /*depth*/0, /*index*/0);
+  auto params = GenericParamList::create(ctx, SourceLoc(), wrapped,
+                                         SourceLoc());
+  auto decl = new (ctx) EnumDecl(SourceLoc(), name, SourceLoc(),
+                                 /*inherited*/{}, params, fileForLookups);
+  wrapped->setDeclContext(decl);
+  return decl;
+}
+
+TestContext::TestContext(ShouldDeclareOptionalTypes optionals)
+    : Ctx(*ASTContext::get(LangOpts, TypeCheckerOpts, SILOpts, SearchPathOpts,
+                           ClangImporterOpts, SymbolGraphOpts, SourceMgr,
+                           Diags)) {
+  registerParseRequestFunctions(Ctx.evaluator);
+  registerTypeCheckerRequestFunctions(Ctx.evaluator);
+  registerClangImporterRequestFunctions(Ctx.evaluator);
+  auto stdlibID = Ctx.getIdentifier(STDLIB_NAME);
+  auto *module = ModuleDecl::create(stdlibID, Ctx);
+  Ctx.addLoadedModule(module);
+
+  FileForLookups = new (Ctx) SourceFile(*module, SourceFileKind::Library,
+                                        /*buffer*/ None);
+  module->addFile(*FileForLookups);
+
+  if (optionals == DeclareOptionalTypes) {
+    SmallVector<Decl *, 2> optionalTypes;
+    optionalTypes.push_back(createOptionalType(
+        Ctx, FileForLookups, Ctx.getIdentifier("Optional")));
+    optionalTypes.push_back(createOptionalType(
+        Ctx, FileForLookups, Ctx.getIdentifier("ImplicitlyUnwrappedOptional")));
+
+    auto result = SourceFileParsingResult{
+        Ctx.AllocateCopy(optionalTypes), /*tokens*/ None,
+        /*interfaceHash*/ None, /*syntaxRoot*/ None};
+
+    Ctx.evaluator.cacheOutput(ParseSourceFileRequest{FileForLookups},
+                              std::move(result));
+  }
+}
