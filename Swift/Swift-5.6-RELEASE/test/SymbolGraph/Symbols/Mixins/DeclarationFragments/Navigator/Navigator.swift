f4733da19b3f995357d1f76d15badfa5eeafd972
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-11 16:29:33 -0700

    1 sub-directory [82 files]
    1 sub-directory for this directory, containing 82 files in Swift format.

---
+++ Swift/Swift-5.6-RELEASE/test/SymbolGraph/Symbols/Mixins/DeclarationFragments/Navigator/Navigator.swift
@@ -0,0 +1,93 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-build-swift %s -module-name Navigator -emit-module -emit-module-path %t/
+// RUN: %target-swift-symbolgraph-extract -module-name Navigator -I %t -pretty-print -output-dir %t
+// RUN: %FileCheck %s --input-file %t/Navigator.symbols.json --check-prefix=MYSTRUCT
+// RUN: %FileCheck %s --input-file %t/Navigator.symbols.json --check-prefix=FOO
+// RUN: %FileCheck %s --input-file %t/Navigator.symbols.json --check-prefix=BAR
+public struct MyStruct<S> { public var x: S
+  public init(x: S) {
+    self.x = x
+  }
+  public func foo() where S: Sequence {}
+  public func bar<T>(x: T) where T: Sequence {}
+}
+
+// MYSTRUCT-LABEL: "precise": "s:9Navigator8MyStructV"
+// MYSTRUCT:        names
+// MYSTRUCT-NEXT:      "title": "MyStruct",
+// MYSTRUCT-NEXT:      "navigator": [
+// MYSTRUCT-NEXT:          {
+// MYSTRUCT-NEXT:              "kind": "identifier"
+// MYSTRUCT-NEXT:              "spelling": "MyStruct"
+// MYSTRUCT-NEXT:          }
+// MYSTRUCT-NEXT:      ]
+
+// when `navigator` and `subHeading` would be identical, the former is elided
+
+// FOO-LABEL: "precise": "s:9Navigator8MyStructV3fooyySTRzlF"
+// FOO:        names
+// FOO-NEXT:      "title": "foo()",
+// FOO-NEXT:      "subHeading": [
+// FOO-NEXT:          {
+// FOO-NEXT:              "kind": "keyword"
+// FOO-NEXT:              "spelling": "func"
+// FOO-NEXT:          }
+// FOO-NEXT:          {
+// FOO-NEXT:              "kind": "text"
+// FOO-NEXT:              "spelling": " "
+// FOO-NEXT:          }
+// FOO-NEXT:          {
+// FOO-NEXT:              "kind": "identifier"
+// FOO-NEXT:              "spelling": "foo"
+// FOO-NEXT:          }
+// FOO-NEXT:          {
+// FOO-NEXT:              "kind": "text"
+// FOO-NEXT:              "spelling": "()"
+// FOO-NEXT:          }
+// FOO-NEXT:      ]
+
+// BAR-LABEL: "precise": "s:9Navigator8MyStructV3bar1xyqd___tSTRd__lF"
+// BAR:        names
+// BAR-NEXT:      "title": "bar(x:)",
+// BAR-NEXT:      "subHeading": [
+// BAR-NEXT:          {
+// BAR-NEXT:            "kind": "keyword",
+// BAR-NEXT:            "spelling": "func"
+// BAR-NEXT:          }
+// BAR-NEXT:          {
+// BAR-NEXT:            "kind": "text",
+// BAR-NEXT:            "spelling": " "
+// BAR-NEXT:          }
+// BAR-NEXT:          {
+// BAR-NEXT:            "kind": "identifier",
+// BAR-NEXT:            "spelling": "bar"
+// BAR-NEXT:          }
+// BAR-NEXT:          {
+// BAR-NEXT:            "kind": "text",
+// BAR-NEXT:            "spelling": "<"
+// BAR-NEXT:          }
+// BAR-NEXT:          {
+// BAR-NEXT:            "kind": "genericParameter",
+// BAR-NEXT:            "spelling": "T"
+// BAR-NEXT:          }
+// BAR-NEXT:          {
+// BAR-NEXT:            "kind": "text",
+// BAR-NEXT:            "spelling": ">("
+// BAR-NEXT:          }
+// BAR-NEXT:          {
+// BAR-NEXT:            "kind": "externalParam",
+// BAR-NEXT:            "spelling": "x"
+// BAR-NEXT:          }
+// BAR-NEXT:          {
+// BAR-NEXT:            "kind": "text",
+// BAR-NEXT:            "spelling": ": "
+// BAR-NEXT:          }
+// BAR-NEXT:          {
+// BAR-NEXT:            "kind": "typeIdentifier",
+// BAR-NEXT:            "spelling": "T"
+// BAR-NEXT:          }
+// BAR-NEXT:          {
+// BAR-NEXT:            "kind": "text",
+// BAR-NEXT:            "spelling": ")"
+// BAR-NEXT:          }
+// BAR-NEXT:      ]
---
+++ Swift/Swift-5.6-RELEASE/test/SymbolGraph/Symbols/SkipsPublicUnderscore.swift
@@ -0,0 +1,88 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-build-swift %s -module-name SkipsPublicUnderscore -emit-module -emit-module-path %t/
+// RUN: %target-swift-symbolgraph-extract -module-name SkipsPublicUnderscore -I %t -pretty-print -output-dir %t
+// RUN: %FileCheck %s --input-file %t/SkipsPublicUnderscore.symbols.json --check-prefix PUBLIC
+
+// RUN: %target-swift-symbolgraph-extract -module-name SkipsPublicUnderscore -I %t -pretty-print -output-dir %t -minimum-access-level internal
+// RUN: %FileCheck %s --input-file %t/SkipsPublicUnderscore.symbols.json --check-prefix INTERNAL
+
+// RUN: %target-swift-symbolgraph-extract -module-name SkipsPublicUnderscore -I %t -pretty-print -output-dir %t -minimum-access-level private
+// RUN: %FileCheck %s --input-file %t/SkipsPublicUnderscore.symbols.json --check-prefix PRIVATE
+
+public protocol PublicProtocol {}
+
+public class SomeClass {
+  // underscored names marked `internal` or tighter should be considered `private`
+
+  // PUBLIC-NOT: "precise": "s:21SkipsPublicUnderscore9SomeClassC12_InternalVarSSvp"
+  // INTERNAL-NOT: "precise": "s:21SkipsPublicUnderscore9SomeClassC12_InternalVarSSvp"
+  // PRIVATE: "precise": "s:21SkipsPublicUnderscore9SomeClassC12_InternalVarSSvp"
+  internal var _InternalVar: String = ""
+}
+
+// PUBLIC-NOT: precise:{{.*}}_ProtocolShouldntAppear
+// PUBLIC-NOT: precise:{{.*}}PublicProtocol
+@_show_in_interface
+public protocol _ProtocolShouldntAppear {}
+
+// PUBLIC-NOT: _ShouldntAppear
+// INTERNAL-DAG: _ShouldntAppear
+
+// INTERNAL-DAG: "precise": "s:21SkipsPublicUnderscore23_ProtocolShouldntAppearP"
+public struct _ShouldntAppear: PublicProtocol, _ProtocolShouldntAppear {
+  // Although these are public and not underscored,
+  // they are inside an underscored type,
+  // so shouldn't be allowed through.
+
+  // PUBLIC-NOT: shouldntAppear
+  // INTERNAL-DAG: shouldntAppear
+  public var shouldntAppear: Int
+
+  // PUBLIC-NOT: InnerShouldntAppear
+  // INTERNAL-DAG: InnerShouldntAppear
+  public struct InnerShouldntAppear {
+
+  // PUBLIC-NOT: InnerInnerShouldntAppear
+  // INTERNAL-DAG: InnerInnerShouldntAppear
+  public struct InnerInnerShouldntAppear {}
+  }
+}
+
+// A public type's relationship to an "internal" protocol
+// shouldn't cause it to be included.
+public struct ShouldAppear: _ProtocolShouldntAppear {}
+
+public struct PublicOuter {
+  // Nor should an "internal" type's relationship to a "public" protocol.
+  // PUBLIC-NOT: _InnerShouldntAppear
+  // INTERNAL-DAG: _InnerShouldntAppear
+  public struct _InnerShouldntAppear: PublicProtocol {}
+}
+
+extension PublicOuter {
+  // PUBLIC-NOT: _FromExtension
+  // INTERNAL-DAG: _FromExtension
+  public struct _FromExtension: PublicProtocol {
+    // PUBLIC-NOT: shouldntAppear
+    // INTERNAL-DAG: shouldntAppear
+    public var shouldntAppear: Int
+  }
+}
+
+extension _ShouldntAppear {
+  // PUBLIC-NOT: FromExtension
+  // INTERNAL-DAG: FromExtension
+  public struct FromExtension: PublicProtocol {
+    // PUBLIC-NOT: shouldntAppear
+    // INTERNAL-DAG: shouldntAppear
+    public var shouldntAppear: Int
+  }
+}
+
+extension _ShouldntAppear.InnerShouldntAppear {
+  public struct ShouldntAppear {}
+}
+
+extension _ShouldntAppear.InnerShouldntAppear: Equatable {}
+
+// PUBLIC: "relationships": []
