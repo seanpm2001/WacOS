f53408a7d513d6f17971570dcb8a5ea6f71b62f2
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-11 17:44:52 -0700

    1 sub-directory [40 files]
    1 sub-directory for this directory, along with 40 files in various file formats.

---
+++ Swift/Swift-5.6-RELEASE/test/SourceKit/DocSupport/doc_error_domain.swift
@@ -0,0 +1,32 @@
+// REQUIRES: OS=macosx
+// RUN: %sourcekitd-test -req=doc-info -module MyError -- -I %S/Inputs \
+// RUN:         -sdk %sdk | %sed_clean > %t.response
+// RUN: %FileCheck -input-file=%t.response %s
+
+// CHECK: struct MyError : CustomNSError, Hashable, Error {
+// CHECK:     enum Code : Int32, @unchecked Sendable, Equatable {
+// CHECK:         case errFirst
+// CHECK:         case errSecond
+// CHECK:     }
+// CHECK:     static var errFirst: MyError.Code { get }
+// CHECK:     static var errSecond: MyError.Code { get }
+
+// CHECK:         key.kind: source.lang.swift.decl.struct,
+// CHECK-NEXT:    key.name: "MyError",
+// CHECK-NEXT:    key.usr: "s:SC11MyErrorCodeLeV",
+// CHECK-NOT:     This is my cool error code.
+
+// CHECK:             key.kind: source.lang.swift.decl.enum,
+// CHECK-NEXT:        key.name: "Code",
+// CHECK-NEXT:        key.usr: "c:@E@MyErrorCode",
+// CHECK-NEXT:        This is my cool error code.
+
+// CHECK:                 key.kind: source.lang.swift.decl.enumelement,
+// CHECK-NEXT:            key.name: "errFirst",
+// CHECK-NEXT:            key.usr: "c:@E@MyErrorCode@MyErrFirst",
+// CHECK-NEXT:            This is first error.
+
+// CHECK:             key.kind: source.lang.swift.decl.var.static,
+// CHECK-NEXT:        key.name: "errFirst",
+// CHECK-NEXT:        key.usr: "s:SC11MyErrorCodeLeV8errFirstSoAAVvpZ",
+// CHECK-NEXT:        This is first error.
