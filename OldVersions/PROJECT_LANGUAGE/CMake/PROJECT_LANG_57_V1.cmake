f9e9de5e39b68b098456260501aadc18960dadda
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-11 19:12:01 -0700

    PROJECT_LANG_57_V1.cmake
    An archive of the first version of the 57th project language file for this project.

---
+++ OldVersions/PROJECT_LANGUAGE/CMake/PROJECT_LANG_57_V1.cmake
@@ -0,0 +1,13 @@
+cmake_minimum_required(VERSION 2.4)
+# Start of script
+# Project language 57
+# For: WacOS
+# About:
+# I decided to make CMake the 57th project language file for this project (WacOS) as CMake is used in some newer source code on this project. It is getting its own project language file, starting here
+
+# File info
+# File type: CMake source file (*.cmake)
+# File version: 1 (2022, Monday, July 11th at 7:11 pm PST)
+# Line count (including blank lines amd compiler line): 14
+
+# End of script
