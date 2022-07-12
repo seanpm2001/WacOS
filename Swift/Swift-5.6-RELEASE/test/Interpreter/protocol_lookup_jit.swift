9bfaabab73535b52a674c24241832023d4279292
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-11 17:33:37 -0700

    Batch 2 - 74 files [final batch]
    The 2nd [and final] batch of files [out of 2] for this directory, containing a final set of 74 files in Swift format.

---
+++ Swift/Swift-5.6-RELEASE/test/Interpreter/protocol_lookup_jit.swift
@@ -0,0 +1,6 @@
+// Test protocol_lookup.swift in JIT mode.
+// RUN: %target-jit-run %S/protocol_lookup.swift | %FileCheck %S/protocol_lookup.swift
+// REQUIRES: executable_test
+
+// REQUIRES: swift_interpreter
+
