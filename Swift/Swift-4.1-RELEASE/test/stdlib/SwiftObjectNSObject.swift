96e5fad431551015c9569f5740710ecc2081a17f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-12 17:29:44 -0700

    Batch 2 - 71 files [final batch]
    The 2nd [and final] batch of files [out of 2] for this directory, containing a final set of 71 files in various file formats.

---
+++ Swift/Swift-4.1-RELEASE/test/stdlib/SwiftObjectNSObject.swift
@@ -0,0 +1,46 @@
+//===--- SwiftObjectNSObject.swift - Test SwiftObject's NSObject interop --===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+// RUN: %empty-directory(%t)
+// 
+// RUN: %target-clang %S/Inputs/SwiftObjectNSObject/SwiftObjectNSObject.m -c -o %t/SwiftObjectNSObject.o -g
+// RUN: %target-build-swift %s -I %S/Inputs/SwiftObjectNSObject/ -Xlinker %t/SwiftObjectNSObject.o -o %t/SwiftObjectNSObject
+// RUN: %target-run %t/SwiftObjectNSObject 2>&1 | %FileCheck %s
+// REQUIRES: executable_test
+
+// REQUIRES: objc_interop
+
+import Foundation
+
+class C { 
+  @objc func cInstanceMethod() -> Int { return 1 }
+  @objc class func cClassMethod() -> Int { return 2 }
+  @objc func cInstanceOverride() -> Int { return 3 }
+  @objc class func cClassOverride() -> Int { return 4 }
+}
+class D : C {
+  @objc func dInstanceMethod() -> Int { return 5 }
+  @objc class func dClassMethod() -> Int { return 6 }
+  @objc override func cInstanceOverride() -> Int { return 7 }
+  @objc override class func cClassOverride() -> Int { return 8 }
+}
+
+@_silgen_name("TestSwiftObjectNSObject") 
+func TestSwiftObjectNSObject(_ c: C, _ d: D)
+
+// This check is for NSLog() output from TestSwiftObjectNSObject().
+// CHECK: c ##SwiftObjectNSObject.C##
+// CHECK-NEXT: d ##SwiftObjectNSObject.D##
+// CHECK-NEXT: S ##SwiftObject##
+
+TestSwiftObjectNSObject(C(), D())
+// does not return
