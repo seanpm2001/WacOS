6ab6b0e29f6f34d4889d7415ec7aea35f513357f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-12 17:08:33 -0700

    1 sub-directory [79 files]
    1 sub-directory for this directory, along with 79 files in various file formats.

---
+++ Swift/Swift-4.1-RELEASE/test/NameBinding/named_lazy_member_loading_swift_struct.swift
@@ -0,0 +1,17 @@
+// RUN: rm -rf %t && mkdir -p %t/stats-pre && mkdir -p %t/stats-post
+//
+// Compile swiftmodule with decl member name tables
+// RUN: %target-swift-frontend -emit-module -o %t/NamedLazyMembers.swiftmodule %S/Inputs/NamedLazyMembers/NamedLazyMembers.swift
+//
+// Check that named-lazy-member-loading reduces the number of Decls deserialized
+// RUN: %target-swift-frontend -typecheck -I %t -disable-named-lazy-member-loading -typecheck -stats-output-dir %t/stats-pre %s
+// RUN: %target-swift-frontend -typecheck -I %t -stats-output-dir %t/stats-post %s
+// RUN: %utils/process-stats-dir.py --evaluate-delta 'NumDeclsDeserialized < -5' %t/stats-pre %t/stats-post
+
+// REQUIRES: rdar_35639403
+
+import NamedLazyMembers
+
+public func test(b: BaseStruct) {
+  let _ = b.memberFunc1()
+}
