72abc67d6b080ca38ac184412f10f80db4cad39b
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-12 17:18:45 -0700

    6 sub-directories [100 files]
    6 sub-directories for this directory, containing 100 files in various file formats.

---
+++ Swift/Swift-4.1-RELEASE/test/SourceKit/DocSupport/doc_clang_module.swift
@@ -0,0 +1,7 @@
+// REQUIRES: objc_interop
+// RUN: %sourcekitd-test -req=doc-info -module Foo -- -F %S/../Inputs/libIDE-mock-sdk \
+// RUN:         %mcp_opt %clang-importer-sdk | %sed_clean > %t.response
+// RUN: diff -u %s.response %t.response
+//
+// REQUIRES: rdar35799113
+//
