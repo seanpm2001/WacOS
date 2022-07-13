63e6aa33885331f8bdc3624742ae808905e02093
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-13 14:15:40 -0700

    PROJECT_LANG_58_V1.bat
    An archive of the first version of the 58th project language file for this project.

---
+++ OldVersions/PROJECT_LANGUAGE/Batchfile/PROJECT_LANG_58_V1.bat
@@ -0,0 +1,10 @@
+REM Start of script
+@ECHO OFF
+REM I decided to make Batchfile (DOS Batch) the 58th project language file for this project (WacOS) as Batchfile is used in some newer source code on this project. It is getting its own project language file, starting here
+ECHO "I decided to make Batchfile (DOS Batch) the 58th project language file for this project (WacOS) as Batchfile is used in some newer source code on this project. It is getting its own project language file, starting here"
+PAUSE
+REM File info
+REM File version: 1 (2022, Wednesday, July 13th at 2:14 pm)
+REM File type: MS-DOS Batch file (*.bat)
+REM Line count (including blank lines and compiler line): 11
+REM End of script
